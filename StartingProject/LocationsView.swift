816945ec9593c660116c94138a0b4ca3e3b5fca6
lukasyano <toliusislukas@yahoo.com>
2023-10-23 15:44:53 +0100

    Initial Commit

---
+++ StartingProject/LocationsView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct LocationsView: View {
+    @EnvironmentObject var model: Model
+    var body: some View {
+        VStack {
+            LittleLemonLogo().padding(.top, 50)
+
+            Text(model.displayingReservationForm ? "Reservation Details" : "Select a location")
+                .padding([.leading, .trailing], 40)
+                .padding([.top, .bottom], 8)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(20)
+
+            NavigationView {
+                List {
+                    ForEach(model.restaurants, id: \.self) { restaurant in
+                        NavigationLink(destination: ReservationForm(restaurant)) {
+                            RestaurantView(restaurant)
+                        }
+                    }
+                }.scrollContentBackground(.hidden)
+
+                    .navigationBarTitle("")
+                    .navigationBarHidden(true)
+            }
+        }
+        .padding(.top, -10)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsView().environmentObject(Model())
+    }
+}
